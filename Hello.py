35eb32047aed0d16727ff00b2b624c2621b651b7
Vasant Verma <Vasant.Verma@anbglobaldc.com>
2019-08-06 15:22:42 +0530

    Added a print statement in Hello.py file

--- Hello.py
+++ Hello.py
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 print("Hi Github. I am alice. ")
 print("Alice is  a active user of Github community")
+=======
+print("Hi Github. I am Bob")
+print("Bob is a active user of Github community")
+>>>>>>> Added a print statement in Hello.py file
